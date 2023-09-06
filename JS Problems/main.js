0f5bdcea86c5aa2cbadb7f8f11ed37d27e0387b5
Dallas <dallas.mcgroarty@gmail.com>
2023-09-05 17:14:57 -0700

    JS ds/algs

--- JS Problems/main.js
+++ JS Problems/main.js
@@ -556,7 +556,7 @@ class Stack {
     }
 
     remove() {
-        this.#data.pop();
+        return this.#data.pop();
     }
 
     top() {
@@ -578,3 +578,296 @@ stack.show();
 stack.remove();
 console.log(stack.top());
 stack.show();
+
+console.log('--- Queue from stacks ---')
+/**
+ * Create Queue from Stack
+ * 
+ * - implement a queue  data structure using two stacks. Do not create an array inside the queue class.
+ *   The queue should implement methods 'add', 'remove', and 'peek'
+ * 
+ */
+
+class QueueStack {
+    #stack1 = new Stack();
+    #stack2 = new Stack();
+
+    constructor() {
+    }
+
+    add(val) {
+        this.#stack1.add(val);
+    }
+
+    remove() {
+        while (this.#stack1.top()) {
+            this.#stack2.add(this.#stack1.remove())
+        }
+
+        this.#stack2.remove();
+    }
+
+    peek() {
+        while (this.#stack1.top()) {
+            this.#stack2.add(this.#stack1.remove())
+        }
+
+        return this.#stack2.top();
+    }
+}
+
+const qs = new QueueStack();
+qs.add(1);
+qs.add(2);
+qs.add(3);
+qs.remove();
+console.log(qs.peek());
+
+console.log('--- Linked List ---')
+/**
+ * Linked lists and Node
+ */
+
+class Node {
+    constructor(data=null,next=null) {
+        this.data = data;
+        this.next = next;
+    }
+}
+
+class LinkedList {
+    constructor(head=null) {
+        this.head = head;
+        this.length = 0;
+    }
+
+    insertFirst(data) {
+        const node = new Node(data);
+
+        if (this.head === null) {
+            this.head = node;
+        } else {
+            let temp = this.head;
+            this.head = node;
+            this.head.next = temp;
+        }
+
+        this.length += 1;
+    }
+
+    size() {
+        let curNode = this.head;
+        let count = 0;
+        while(curNode) {
+            count += 1;
+            curNode = curNode.next;
+        }
+        return count;
+    }
+
+    getFirst() {
+        return this.head;
+    }
+
+    getLast() {
+        if (!this.head) {
+            return null;
+        }
+
+        let curNode = this.head;
+        while(curNode.next) {
+            curNode = curNode.next;
+        }
+        return curNode;
+    }
+
+    clear() {
+        this.head = null;
+    }
+
+    removeFirst() {
+        if (!this.head) {
+            return
+        }
+
+        this.head = this.head.next;
+    }
+
+    removeLast() {
+        if (!this.head) {
+            return;
+        }
+
+        if (!this.head.next) {
+            this.head = null;
+            return;
+        }
+
+        let curNode = this.head;
+        let prevNode = this.head.next;
+        while (curNode.next) {
+            prevNode = curNode;
+            curNode = curNode.next;
+        }
+
+        prevNode.next = null;
+    }
+
+    insertLast(data) {
+        const node = new Node(data);
+
+        if (!this.head) {
+            this.head = node;
+            return;
+        }
+
+        let curNode = this.head;
+
+        while(curNode.next) {
+            curNode = curNode.next;
+        }
+
+        curNode.next = node;
+    }
+
+    getAt(index) {
+        let counter = 0;
+        let curNode = this.head;
+        while(curNode) {
+            if (counter == index) {
+                return curNode;
+            } 
+            counter += 1;
+            curNode = curNode.next;
+        }
+        
+        return null;
+    }
+
+    removeAt(index) {
+        if (!this.head) {
+            return;
+        }
+
+       if (index == 0) {
+            this.head = this.head.next;
+            return;
+       }
+
+        let prev = this.head;
+        let next = this.head.next;
+        let counter = 0;
+
+        while (next) {
+            if (counter === index - 1) {
+                prev.next = next.next;
+                return;
+            }
+            counter += 1;
+            prev = next;
+            next = next.next;
+        }
+    }
+
+    insertAt(index,data) {
+        const node = new Node(data);
+
+        if (index == 0) {
+            let temp = this.head;
+            this.head = node;
+            this.head.next = temp;
+        }
+
+        let prev = this.head;
+        let next = this.head.next;
+        let counter = 0;
+
+        while (next) {
+            if (counter === index - 1) {
+                node.next = next;
+                prev.next = node;
+                return;
+            }
+            counter += 1;
+            prev = next;
+            next = next.next;
+        }
+    }
+}
+
+const n1 = new Node(1);
+const n2 = new Node(2);
+const n3 = new Node(3);
+const list = new LinkedList();
+list.insertFirst(1);
+list.insertFirst(2);
+list.insertFirst(3);
+console.log(list);
+console.log('list size -> ', list.size());
+console.log('list get first -> ', list.getFirst());
+console.log('list get last -> ', list.getLast());
+//list.removeFirst();
+//list.removeLast(); // remove 1, now 2 is last
+//console.log('list get last after remove -> ',list.getLast());
+list.insertLast(4);
+//console.log('list get last after insert last', list.getLast());
+console.log('list get at index -> ', list.getAt(2));
+// list = 3,2,1,4
+//list.removeAt(0);
+//console.log('list get at index -> ', list.getAt(0));
+
+// list = 3,2,1,4
+list.insertAt(3,6);
+console.log('list get at index -> ', list.getAt(3))
+
+
+/**
+ * Mid point of linked list
+ * - return middle of linked list if list has an even number of elements, return the node at the end of the first half.
+ *   Do not use counter variable, do not retrieve size of list, only iterate through list one time.
+ */
+console.log('--- list mid point ---');
+
+function midpoint(list) {
+    let slow = list.getFirst();
+    let fast = list.getFirst();
+
+    while (fast.next && fast.next.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    return slow;
+}
+// list = 3,2,1,6,4
+console.log(midpoint(list));
+
+/**
+ * Circular linked list
+ *  - return true if linked list is circular, else return false
+ */
+console.log('--- circular linked list ---');
+function circular(list) {
+    let slow = list.getFirst();
+    let fast = list.getFirst();
+
+    while (fast.next && fast.next.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+
+        if (slow === fast) {
+            return true;
+        }
+    }
+
+    return false;
+}
+const l1 = new LinkedList();
+const a = new Node('a');
+const b = new Node('b');
+const c = new Node('c');
+l1.head = a;
+a.next = b;
+b.next = c;
+c.next = a;
+console.log(circular(l1));
